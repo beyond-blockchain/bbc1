cb7bc13e36349d34713a6c6e513b4ba8943d14de
Michihito Kato <mkato@zettant.com>
2018-05-09 17:20:12 +0900

    fix: failed to compile libbbcsig.so on Ubuntu 16.04

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/openssl/openssl.git third_party/openssl
 cd third_party/openssl
+git checkout f70425d3ac5e4ef17cfa116d99f8f03bbac1c7f2
 ./config && make
 
 cd ../libbbcsig
