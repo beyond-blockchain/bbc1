fed9887ad78ba0556d3fcb431af3e406dd901153
Jun Kurihara <kurihara@ieee.org>
2018-05-14 19:34:57 +0900

    Fixed: modified prepare.sh so as to pass the travis ci check.

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 git clone https://github.com/openssl/openssl.git third_party/openssl
-pushd third_party/openssl
+cd third_party/openssl
 git checkout f70425d3ac5e4ef17cfa116d99f8f03bbac1c7f2
 ./config && make
-popd
 
-pushd bbc1/core/libbbcsig
+cd ../../bbc1/core/libbbcsig
 make clean
 make
-popd
