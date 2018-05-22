648313f95427f6cef1d7d9630a31e5ce995c5911
Michihito Kato <mkato@zettant.com>
2018-05-22 13:15:33 +0900

    fix: failed to create libbbcsig.so (#89)

--- prepare.py
+++ prepare.py
@@ -5,5 +5,5 @@ install_pkg_dir = os.path.join(sitedir, 'bbc1')
 target_dir = os.path.join(install_pkg_dir, 'core')
 os.makedirs(target_dir, exist_ok=True)
 dst_path = os.path.join(target_dir, 'libbbcsig.so')
-shutil.copy('third_party/libbbcsig.so', dst_path)
+shutil.copy('libs/libbbcsig.so', dst_path)
 
--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-git clone https://github.com/openssl/openssl.git third_party/openssl
-cd third_party/openssl
+git clone https://github.com/openssl/openssl.git libs/openssl
+cd libs/openssl
 git checkout f70425d3ac5e4ef17cfa116d99f8f03bbac1c7f2
 ./config && make
 
-cd ../../bbc1/core/libbbcsig
+cd ../libbbcsig
 make clean
 make
