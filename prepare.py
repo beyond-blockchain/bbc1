8ab45616b53c838497469d69934ecf04d57cb8f9
Michihito Kato <mkato@zettant.com>
2018-06-11 01:59:45 +0900

    fix : install error caused by cryptography fixed version (#92)

--- prepare.py
+++ prepare.py
@@ -1,6 +1,15 @@
 import os, shutil, site
 
-sitedir = site.getsitepackages()[0]
+sitedir = None
+if hasattr(site, 'getsitepackages'):
+    # normal execution
+    sitepackages = site.getsitepackages()
+    sitedir = sitepackages[0]
+else:
+    # workaround for virtualenv
+    from distutils.sysconfig import get_python_lib
+    sitepackages = [get_python_lib()]
+    sitedir = sitepackages[0]
 install_pkg_dir = os.path.join(sitedir, 'bbc1')
 target_dir = os.path.join(install_pkg_dir, 'core')
 os.makedirs(target_dir, exist_ok=True)
