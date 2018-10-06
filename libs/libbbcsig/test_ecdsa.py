2e19995fd4f0246a3963ece8fd73f82e05e339a9
Takeshi Kubo <takeshi@quvox.net>
2018-10-07 00:16:45 +0900

    update test code

--- libs/libbbcsig/test_ecdsa.py
+++ libs/libbbcsig/test_ecdsa.py
@@ -1,11 +1,16 @@
+import platform
 import binascii
 import os
 from ctypes import *
 
-if os.name == "nt":
+if platform.system() == "Windows":
     lib = windll.LoadLibrary("libbbcsig.dll")
+elif platform.system() == "Darwin":
+    lib = cdll.LoadLibrary("libbbcsig.dylib")
 else:
     lib = cdll.LoadLibrary("libbbcsig.so")
+
+CURVETYPE = 2  # CURVE_TYPE_P256 in libbbcsig.h
 
 test_digest = binascii.a2b_hex("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 privkey_len = c_int32(32)
@@ -16,7 +21,7 @@ pubkey      = (c_byte * pubkey_len.value)()
 signature = (c_byte * 64)()
 
 print("# -- generate_keypair()")
-lib.generate_keypair(0, byref(pubkey_len), pubkey, byref(privkey_len), privkey)
+lib.generate_keypair(CURVETYPE, 0, byref(pubkey_len), pubkey, byref(privkey_len), privkey)
 print("private_key:", binascii.b2a_hex(privkey), ", len=", privkey_len)
 print("public_key:", binascii.b2a_hex(pubkey), ", len=", pubkey_len)
 
@@ -25,7 +30,7 @@ sig_r = (c_byte * 32)()
 sig_s = (c_byte * 32)()
 sig_r_len = (c_byte * 4)()  # Adjust size according to the expected size of sig_r and sig_s. Default:uint32.
 sig_s_len = (c_byte * 4)()
-print(lib.sign(privkey_len, privkey, privkey_len, test_digest, sig_r, sig_s, sig_r_len, sig_s_len))
+print(lib.sign(CURVETYPE, privkey_len, privkey, privkey_len, test_digest, sig_r, sig_s, sig_r_len, sig_s_len))
 sig_r_len = int.from_bytes(bytes(sig_r_len), "little")
 sig_s_len = int.from_bytes(bytes(sig_s_len), "little")
 sig_r = binascii.a2b_hex("00" * (32 - sig_r_len) + bytes(sig_r)[:sig_r_len].hex())
@@ -35,7 +40,7 @@ print(binascii.b2a_hex(signature))
 
 print("# -- output_der()")
 der_data = (c_byte * 512)()     # 256 -> 512
-der_len = lib.output_der(privkey_len, privkey, byref(der_data))
+der_len = lib.output_der(CURVETYPE, privkey_len, privkey, byref(der_data))
 print("DER: len=", der_len, "  dat=", binascii.b2a_hex(bytearray(der_data)[:der_len]))
 
 print("# -- clear private key")
@@ -43,7 +48,7 @@ privkey = (c_byte * privkey_len.value)()
 print("private_key:", binascii.b2a_hex(privkey), ", len=", privkey_len)
 
 print("# -- convert_from_der()")
-lib.convert_from_der(der_len, byref(der_data), 0, byref(pubkey_len), pubkey, byref(privkey_len), privkey)
+lib.convert_from_der(CURVETYPE, der_len, byref(der_data), 0, byref(pubkey_len), pubkey, byref(privkey_len), privkey)
 print("private_key:", binascii.b2a_hex(privkey), ", len=", privkey_len)
 print("public_key:", binascii.b2a_hex(pubkey), ", len=", pubkey_len)
 
@@ -52,7 +57,7 @@ sig_r = (c_byte * 32)()
 sig_s = (c_byte * 32)()
 sig_r_len = (c_byte * 4)()  # Adjust size according to the expected size of sig_r and sig_s. Default:uint32.
 sig_s_len = (c_byte * 4)()
-print(lib.sign(privkey_len, privkey, privkey_len, test_digest, sig_r, sig_s, sig_r_len, sig_s_len))
+print(lib.sign(CURVETYPE, privkey_len, privkey, privkey_len, test_digest, sig_r, sig_s, sig_r_len, sig_s_len))
 sig_r_len = int.from_bytes(bytes(sig_r_len), "little")
 sig_s_len = int.from_bytes(bytes(sig_s_len), "little")
 sig_r = binascii.a2b_hex("00" * (32 - sig_r_len) + bytes(sig_r)[:sig_r_len].hex())
@@ -62,23 +67,23 @@ print(binascii.b2a_hex(signature))
 
 print("# -- get compressed pubkey")
 pubkey = (c_byte * 33)()
-lib.get_public_key_compressed(privkey_len, privkey, byref(pubkey_len), pubkey)
+lib.get_public_key_compressed(CURVETYPE, privkey_len, privkey, byref(pubkey_len), pubkey)
 print("public_key:", binascii.b2a_hex(pubkey), ", len=", pubkey_len)
 
 print("# -- verify with compressed pubkey")
-res = lib.verify(pubkey_len, pubkey, 32, test_digest, 64, signature)
+res = lib.verify(CURVETYPE, pubkey_len, pubkey, 32, test_digest, 64, signature)
 print("result:", res)
 
 print("# -- get uncompressed pubkey")
 pubkey = (c_byte * 65)()
-lib.get_public_key_uncompressed(privkey_len, privkey, byref(pubkey_len), pubkey)
+lib.get_public_key_uncompressed(CURVETYPE, privkey_len, privkey, byref(pubkey_len), pubkey)
 print("public_key:", binascii.b2a_hex(pubkey), ", len=", pubkey_len)
 
 print("# -- verify with compressed pubkey")
-res = lib.verify(pubkey_len, pubkey, 32, test_digest, 64, signature)
+res = lib.verify(CURVETYPE, pubkey_len, pubkey, 32, test_digest, 64, signature)
 print("result:", res)
 
 print("# -- output_pem()")
 pem = (c_char * 512)()      # 256 -> 512
-length = lib.output_pem(privkey_len, privkey, byref(pem))
+length = lib.output_pem(CURVETYPE, privkey_len, privkey, byref(pem))
 print("PEM: len=", length, " dat=", pem.value)
