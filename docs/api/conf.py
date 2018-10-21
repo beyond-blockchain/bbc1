4467424773945480c9d53f5fc95cd6f4a8f00d62
Takeshi Kubo <takeshi@quvox.net>
2018-10-22 00:46:49 +0900

    update API docs

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, beyond-blockchain.org'
 author = 'beyond-blockchain.org'
 
 # The short X.Y version
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.1'
 
 
 # -- General configuration ---------------------------------------------------
